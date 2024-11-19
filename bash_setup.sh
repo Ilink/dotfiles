527c782e1d8e109572c049a06cf346f024e42dcd
Ian Link <ian@ilink.io>
2024-11-19 13:30:57 +0000

    dont use bashrc by default; add neovim setup

--- bash_setup.sh
+++ bash_setup.sh
@@ -9,9 +9,13 @@ ln -s $PWD/.bash_prompt $HOME/.bash_prompt
 ln -s $PWD/.bash_profile $HOME/.bash_profile
 ln -s $PWD/git-completion.bash $HOME/.git-completion.bash 
 ln -s $PWD/.aliases $HOME/.aliases
-ln -s $PWD/.bashrc $HOME/.bashrc
+#ln -s $PWD/.bashrc $HOME/.bashrc
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
 ln -s $PWD/vim/.vimrc $HOME/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir -p $HOME/.config/nvim
+ln -s $PWD/init.vim $HOME/.config/nvim/init.vim
+ln -s $PWD/coc-settings.json $HOME/.config/nvim/coc-settings.json
